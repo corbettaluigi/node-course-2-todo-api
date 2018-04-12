3359ab3e306a950edaabe23c0152033da2c1424d
Gigi <corbettaluigi@gmail.com>
2018-04-12 20:05:18 +0200

    app setup for heroku - config corrected

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-if (process.env.PORT) {
-  var dbUri = 'mongodb://lcdbuser:lc123db@ds121534.mlab.com:21534/lcdb';
-} else {
-  var dbUri = 'mongodb://localhost:27017/TodoApp';
-}
+// if (process.env.PORT) {
+//   var dbUri = 'mongodb://lcdbuser:lc123db@ds121534.mlab.com:21534/lcdb';
+// } else {
+//   var dbUri = 'mongodb://localhost:27017/TodoApp';
+// }
 
+var dbUri = process.env.DBURI || 'mongodb://localhost:27017/TodoApp';
 mongoose.connect(dbUri);
 
 module.exports = {mongoose};
